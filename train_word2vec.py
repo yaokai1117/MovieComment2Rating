b5dcca70c70e6b70d22f9db185d4e2e031dc6979
Kai Yao <yaokai1117@gmail.com>
2017-04-09 18:47:19 +0800

    Add word2vec embedding.

---
+++ train_word2vec.py
@@ -0,0 +1,22 @@
+import gensim, logging
+import os
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+
+class Sentences(object):
+    """
+    Generate gensim input from plain text files.
+    """
+
+    def __init__(self, dirname):
+        self.dirname = dirname
+
+    def __iter__(self):
+        for fname in os.listdir(self.dirname):
+            for line in open(os.path.join(self.dirname, fname)):
+                yield line.split()
+
+sentences = Sentences("plain")
+model = gensim.models.Word2Vec(sentences)
+model.save("model.bin")
