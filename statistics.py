3e2e422b42afef24244d226a764e5023a730186e
Kai Yao <yaokai1117@gmail.com>
2017-05-19 20:25:34 +0800

    Some changes in my thesis.

--- statistics.py
+++ statistics.py
@@ -2,13 +2,19 @@ import json
 
 if __name__ == '__main__':
     confusion_matrix = [[0 for _ in range(5)] for _ in range(5)]
+    mse = 0
+    cnt = 0
     with open("results\\test.all.txt", encoding="UTF-8") as f:
         for line in f:
             case = json.loads(line)
             prediction = int(float(case["PD"]))
             groud_truth = int(float(case["GT"]))
             confusion_matrix[prediction][groud_truth] += 1
+            cnt += 1
+            mse += (prediction - groud_truth) * (prediction - groud_truth)
 
     for i in range(5):
         print('\n')
         print('\t'.join(str(i) for i in confusion_matrix[i]))
+    mse /= cnt
+    print(mse)
